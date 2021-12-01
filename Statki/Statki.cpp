26f814da5887f46a00cfc1f0a43bfe0cf8de36ae
mpoomirski <michal.pomirski01@gmail.com>
2021-12-02 00:42:44 +0100

    Placing ships on board working, started input handling

--- Statki/Statki.cpp
+++ Statki/Statki.cpp
@@ -3,10 +3,28 @@
 #define SIZE_X 10
 
 typedef struct Position {
-	int x;
 	int y;
+	int x;
 } Position_t;
 
+//Loop for N/W/S/E position system
+Position_t positionLoop(Position_t position, char direction, int increment) {
+	switch (direction) {
+	case 'N':
+		return { position.y + increment, position.x };
+		break;
+	case 'E':
+		return { position.y, position.x - increment };
+		break;
+	case 'S':
+		return { position.y - increment, position.x };
+		break;
+	case 'W':
+		return { position.y, position.x + increment };
+		break;
+	}
+	return { 0, 0 };
+}
 
 //Game board
 class Board {
@@ -14,15 +32,18 @@ public:
 	void initBoard();
 	void printBoard();
 	void placeChar(Position_t, char);
-
-private:
 	char board[SIZE_Y][SIZE_X];
 };
 
 void Board::initBoard() {
 	for (int i = 0; i < SIZE_Y; i++) {
 		for (int j = 0; j < SIZE_X; j++) {
-			board[i][j] = '0';
+			if (i == SIZE_Y / 2) {
+				board[i][j] = 'x';
+			}
+			else {
+				board[i][j] = '0';
+			}
 		}
 	}
 }
@@ -32,6 +53,7 @@ void Board::printBoard() {
 		for (int j = 0; j < SIZE_X; j++) {
 			std::cout << board[i][j] << " ";
 		}
+		std::cout << "\t" << i;
 		std::cout << "\n";
 	}
 }
@@ -42,17 +64,18 @@ void Board::placeChar(Position_t position, char c) {
 
 
 //Battleships
-//
 class Ship {
 public:
 	int size;
 	char ship_class[4];
+	bool is_placed;
 	Ship();
 };
 
 Ship::Ship(){
 	size = 0;
 	strcpy_s(ship_class, "");
+	is_placed = false;
 };
 
 //Player
@@ -63,7 +86,8 @@ public:
 	bool his_turn;
 	Player();
 	void initShips(int default_ship_numbers[4]);
-	void placeShip(Board* board, Ship ship, Position_t position, char direction);
+	Ship* selectShip(int index, char ship_class[4]);
+	void placeShip(Board* board, Position_t position, char direction, int index, char ship_class[4]);
 
 private:
 	bool isValidPlacement(Position_t position, int ship_length, char direction);
@@ -85,7 +109,7 @@ void Player::initShips(int default_ship_numbers[4]) {
 	Ship choose_ships[4] = { carrier, battleship, cruiser, destroyer };
 
 	for (int i = 0; i < (sizeof(choose_ships) / sizeof(Ship)); i++) {
-		choose_ships[i].size = default_ship_numbers[i];
+		choose_ships[i].size = ship_sizes[i];
 		strcpy_s(choose_ships[i].ship_class, names[i]);
 	}
 
@@ -97,11 +121,76 @@ void Player::initShips(int default_ship_numbers[4]) {
 	}
 	
 }
-//DOKOŃCZYĆ TUTAJ!!!!!!!
-void Player::placeShip(Board* board, Ship ship, Position_t position, char direction) {
-	if (isValidPlacement(position, ship.size, direction)) {
-		board->placeChar(position, 'x');
+
+Ship* Player::selectShip(int index, char ship_class[4]) {
+	//I'll refactor this later
+	if (!strcmp(ship_class, "CAR")) {
+		if (ships[0][index].size != 0) {
+			return &ships[0][index];
+		}
+		else {
+			throw "SHIP DOESN'T EXIST";
+		}
 	}
+	else if (!strcmp(ship_class, "BAT")) {
+		if (index <= (sizeof(ships[1]) / sizeof(ships[1][0]))) {
+			return &ships[1][index];
+		}
+	}
+	else if (!strcmp(ship_class, "CRU")) {
+		if (index <= (sizeof(ships[2]) / sizeof(ships[2][0]))) {
+			return &ships[2][index];
+		}
+	}
+	else if (!strcmp(ship_class, "DES")) {
+		if (index <= (sizeof(ships[3]) / sizeof(ships[3][0]))) {
+			return &ships[3][index];
+		}
+	}
+}
+
+void Player::placeShip(Board* board, Position_t position, char direction, int index, char ship_class[4]) {
+	Ship* ship;
+	try {
+		ship = selectShip(index, ship_class);
+	}
+	catch (const char* error) {
+		std::cout << error;
+		return;
+	}
+
+	if (ship->is_placed) {
+		std::cout << "SHIP ALREADY PLACED\n";
+		return;
+	}
+
+	if (isValidPlacement(position, ship->size, direction)) {
+		int count = 1;
+		for (int i = 0; i < ship->size; i++) {
+			//Check if the ship doesn't collide
+			position = positionLoop(position, direction, 1);
+			if (board->board[position.y][position.x] == '0') {
+				count++;
+			}
+		}
+
+		if (count >= ship->size) {
+			for (int i = 0; i < ship->size; i++) {
+				//Draw the ship on the board
+				position = positionLoop(position, direction, -1);
+				board->placeChar(position, 'x');
+			}
+		}
+		else {
+			std::cout << "ERROR PLACING SHIP\n";
+		}
+	}
+	else {
+		std::cout << "ERROR NOT VALID PLACEMENT\n";
+	}
+
+	ship->is_placed = true;
+
 }
 
 bool Player::isValidPlacement(Position_t position, int ship_length, char direction) {
@@ -115,43 +204,92 @@ bool Player::isValidPlacement(Position_t position, int ship_length, char directi
 		max_y = 20;
 	}
 
-	switch (direction) {
-	case 'N':
-		return position.y + ship_length - 1 <= max_y;
-		break;
-	case 'E':
-		return position.x + 1 >= ship_length;
-		break;
-	case 'S':
-		return position.y - ship_length + 1 >= min_y;
-		break;
-	case 'W':
-		return position.x + ship_length <= SIZE_X;
-		break;
+	if ((position.x < SIZE_X && position.x >= 0) && (position.y >= min_y && position.y <= max_y)) {
+		switch (direction) {
+		case 'N':
+			return position.y + ship_length - 1 <= max_y;
+			break;
+		case 'E':
+			return position.x + 1 >= ship_length;
+			break;
+		case 'S':
+			return position.y - ship_length + 1 >= min_y;
+			break;
+		case 'W':
+			return position.x + ship_length <= SIZE_X;
+			break;
+		}
 	}
 
 	return false;
 }
 
+void printHandler(int print_mode, Board* board) {
+	switch (print_mode) {
+	case 0: {
+		board->printBoard();
+		break;
+	}
+	}
+}
 
+void userInputHandler(Board* board, Player* players[2]) {
+	Player* player = players[0];
+
+
+	//TODO: CHOOSING PLAYERS
+	for (int i = 0; i < 2; i++) {
+		if (players[i]->his_turn) {
+			player = players[i];
+		}
+	}
+
+	//Hideous code ahead, Cpp's fault ¯\_ (ツ)_/¯
+	char command[30];
+	while (std::cin >> command) {
+		if (strcmp(command, "[playerA]") == false || strcmp(command, "[playerB]") == false) {
+			player->his_turn = !player->his_turn;
+		}
+
+		if (!strcmp(command, "PRINT")) {
+			int print_mode = 0;
+			std::cin >> print_mode;
+			printHandler(print_mode, board);
+		}
+		else if (!strcmp(command, "PLACE_SHIP")) {
+			int position_y, position_x;
+			char direction;
+			int index;
+			char ship_class[4];
+			std::cin >> position_y >> position_x >> direction >> index >> ship_class;
+			player->placeShip(board, { position_y, position_x }, direction, index, ship_class);
+		}
+	}
+}
+
+//Main
 int main()
 {
 	Board board;
 	board.initBoard();
-	board.printBoard();
 
-	Player A, B;
-	A.name = 'A';
-	B.name = 'B';
+	Player* A = new Player;
+	Player* B = new Player;
+	Player* players[2] = { A, B };
+
+	A->name = 'A';
+	B->name = 'B';
+
 
 	int default_ship_numbers[4] = { 1, 2, 3, 4 };
 
-	A.initShips(default_ship_numbers);
-	B.initShips(default_ship_numbers);
+	A->initShips(default_ship_numbers);
+	B->initShips(default_ship_numbers);
 
-	//dokonczyc tu
-	//A.placeShip(board*, A.ships[0][0], )
+	userInputHandler(&board, players);
 
+	//B.placeShip(&board, B.ships[0][0], { 15, 5 }, 'W');
+	//A.placeShip(&board, A.ships[0][0], { 6, 0 }, 'N');
 
 	return 0;
 }
